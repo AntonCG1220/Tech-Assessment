a419b737b84a51654f60cff591f9fa16fd96dad4
Anton <antonchristophergarcia@gmail.com>
2025-06-17 12:31:49 +0800

    Techinical Assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Field {
+  ID?: string;
+  Type: string;
+  Text?: string;
+  Placeholder?: string;
+  Title?: string;
+  AlertMessage?: string;
+}
+
+interface FormConfig {
+  Title: string;
+  Subtitle: string;
+  Fields: Field[];
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tech-assessment';
+export class AppComponent implements OnInit {
+  config: FormConfig = {
+    Title: 'Tech Assessment',
+    Subtitle: 'Anton Christopher Garcia',
+    Fields: [
+      { Type: 'H1', 
+        Text: 'Person name'
+      },
+        { ID: 'person-name',
+          Type: 'Text', 
+          Placeholder: 'John Smith'
+        },
+          { 
+            ID: 'hello-button', 
+            Type: 'Button', 
+            Title: 'Say hi', 
+            AlertMessage: 'Hello ${person-name}' 
+          }
+    ]
+  };
+
+  formData: { [key: string]: string } = {};
+
+  ngOnInit() {
+    this.config.Fields.forEach(field => {
+      if (field.Type === 'Text' && field.ID) {
+        this.formData[field.ID] = '';
+      }
+    });
+  }
+
+  showAlert(alertMessage: string) {
+    let message = alertMessage;
+    for (const id in this.formData) {
+      message = message.replace(`\${${id}}`, this.formData[id] || '');
+    }
+    alert(message);
+  }
 }

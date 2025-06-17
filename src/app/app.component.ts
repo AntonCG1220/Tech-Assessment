30263ffdcc527bbbc74be679a591c6a7548cfa3a
Anton <antonchristophergarcia@gmail.com>
2025-06-17 18:15:29 +0800

    Tech Assessment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 interface Field {
   ID?: string;
@@ -20,45 +20,44 @@ interface FormConfig {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   config: FormConfig = {
     Title: 'Tech Assessment',
     Subtitle: 'Anton Christopher Garcia',
     Fields: [
-      { Type: 'H1', 
-        Text: 'Person name'
-      },
-        { ID: 'person-name',
-          Type: 'Text', 
-          Placeholder: 'John Smith'
-        },
-          { 
-            ID: 'hello-button', 
-            Type: 'Button', 
-            Title: 'Say hi', 
-            AlertMessage: 'Hello ${person-name}' 
-          }
-    ]
-  };
+      { Type: 'H1',
+         Text: 'Person name'
+         },
+          { ID: 'person-name', 
+            Type: 'Text',
+            Placeholder: 'John Smith'
+          },
+            { ID: 'hello-button',
+              Type: 'Button', 
+              Title: 'Say hi', 
+              AlertMessage: 'Hello ${person-name}' 
+            }
+          ]
+        };
 
-  formData: { [key: string]: string } = {};
+  formGroup = new FormGroup<Record<string, FormControl<string | null>>>({});
 
-  ngOnInit() {
+  constructor() {
     this.config.Fields.forEach(field => {
       if (field.Type === 'Text' && field.ID) {
-        this.formData[field.ID] = '';
+        this.formGroup.addControl(field.ID, new FormControl(''));
       }
     });
   }
 
   showAlert(alertMessage: string) {
     let message = alertMessage;
-    for (const id in this.formData) {
-      message = message.replace(`\${${id}}`, this.formData[id] || '');
+    for (const id of Object.keys(this.formGroup.value)) {
+      message = message.replace(`\${${id}}`, this.formGroup.value[id] || '');
     }
     alert(message);
   }
